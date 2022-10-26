e4633cbb40311123e8e120f308aecec18ae796d2
SKShehan <shehansilva2013@gmail.com>
2022-10-27 04:18:24 +0530

    Product Routes

--- models/Product.js
+++ models/Product.js
@@ -3,14 +3,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  productId: { type: String, required: true },
+  productId: { type: String, required: false },
   productName: { type: String, required: true },
   productStatus: { type: String, required: true },
   productPrice: { type: Number, required: true },
-  productDescription: { type: Number, required: true },
-  supplierId: { type: String, required: true },
+  productDescription: { type: String, required: true },
+  supplierId: { type: String, required: false },
   supplierName: { type: String, required: true },
   quantity: { type: String, required: true },
+  quantityType : { type: String , required: true},
   productImage: { type: String, required: false },
 });
 
